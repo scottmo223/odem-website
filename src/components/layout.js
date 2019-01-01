5acfc6bcb71a09662c2ce5b8b8d751cc842c9142
Scott Morales <morales.scott@gmail.com>
2019-01-01 13:33:31 -0600

    Minor wording changes to layout.js and contact.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import { Container } from 'reactstrap'
 
 import Navbar from './navbar'
 import '../layouts/bootstrap.min.css'
@@ -19,19 +20,12 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Navbar>{data.site.siteMetadata.title}</Navbar>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <Container>
           {children}
           <footer>
             © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
           </footer>
-        </div>
+        </Container>
       </>
     )}
   />
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ import SEO from '../components/seo'
 const ContactUs = () => (
   <Layout>
     <SEO title="Contact Us" />
-    <h1>Hi from the second page</h1>
+    <h1>Send us a message!</h1>
     <Link to="/">Go back to the homepage</Link>
     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdJBx2VN6FXNZI_NuRIsTKoyUZCCg-2ZIRd5jzB8pkkHUhkwg/viewform?embedded=true" width="640" height="1087" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
   </Layout>
