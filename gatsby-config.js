a34828f9650b75d0fd9b34dd65a764bdd145ead8
Scott Morales <morales.scott@gmail.com>
2019-01-17 02:55:33 -0600

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,12 @@ module.exports = {
         icon: `src/images/open_doors_logo_globe.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-132692132-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
      'gatsby-plugin-offline',
