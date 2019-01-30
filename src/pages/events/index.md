78aa3d2df6160b335505109fd90f2ef5e8398bc0
Scott Morales <morales.scott@gmail.com>
2019-01-30 14:53:53 -0600

    Events now queried from graphql

---
+++ src/pages/events/index.md
@@ -0,0 +1,14 @@
+---
+templateKey: 'events'
+title: Upcoming Events
+---
+ 
+Feb 10-11  |  Delicias, Chihuahua
+
+Feb 12-13 | Oaxaca
+
+Feb 14 | Manzanillo, Colima
+
+Feb 15 | Colima, Colima
+
+Feb 15-16 | Gaudalajara, Jal
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container } from 'reactstrap'
 //import { Link } from 'gatsby'
-
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Image from '../components/image'
 import HeaderImage from '../components/headerImage'
@@ -10,49 +10,63 @@ import Carousel from '../components/carousel'
 import Social from '../components/social'
 
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Odem Missions" keywords={[`ODEM`, `missions`, `evangelism`]} />
-    
-    <div style={{position: 'relative', marginBottom: '4rem',}}>
-      <HeaderImage />
-      <h1 style={{ position: 'absolute', top: '50%', left: '50%', fontWeight: 'bold', fontSize: '3.7em', transform: 'translate(-50%, -52%)', width: 'fit-content'}} className='text-dark'>Open Doors Evangelistic Ministries</h1>
-    </div>
-
-    <Container>
+export default ({data}) => {
+  console.log(data.allMarkdownRemark.edges[0].node.html)
+  return (
+    <Layout>
+      <SEO title="Odem Missions" keywords={[`ODEM`, `missions`, `evangelism`]} />
       
-      <div className='d-flex flex-column flex-sm-row justify-content-sm-around align-items-center align-items-sm-stretch'>
-        <div className='card mb-5 mb-sm-0' style={{width: '18rem', height: '13rem'}}>
-          <h4 className='card-title text-white bg-secondary mb-3 p-2'>Social Media</h4>
-          <p className='card-text'>Follow our social media for current events</p>
-          <Social />
-        </div>
-        <div className='card' style={{width: '18rem'}}>
-          <h4 className='card-title text-white bg-secondary mb-0 p-2'>Upcoming Events</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">Jan 23 Saint Paul, MN</li>
-            <li className="list-group-item">Feb 13-15 Madrid, Spain</li>
-            <li className="list-group-item">Jun 10-13 Guadalajara, Mexico</li>
-            <li className="list-group-item">Apr 12-22 Your Church, Somewhere</li>
-          </ul>
-          
-        </div>
+      <div style={{position: 'relative', marginBottom: '4rem',}}>
+        <HeaderImage />
+        <h1 style={{ position: 'absolute', top: '50%', left: '50%', fontWeight: 'bold', fontSize: '3.7em', transform: 'translate(-50%, -52%)', width: 'fit-content'}} className='text-dark'>Open Doors Evangelistic Ministries</h1>
       </div>
 
-      <div style= {{ margin: `5rem 0rem` }}>
-        <Carousel />
-      </div>
-      <hr />
-      <div style={{ margin: `5rem 4rem 5rem 3rem` }}>
-        <h1>Our Mission</h1>
-        <p>Our mission is to encourage churches in several nations, by support and training in evangelism and discipleship, to achieve the Great Commission.</p>
-      </div>
-      <hr />
-        <div style={{ maxWidth: `400px`, margin: '4rem auto' }}>
-          <Image />
+      <Container>
+        
+        <div className='d-flex flex-column flex-sm-row justify-content-sm-around align-items-center align-items-sm-stretch'>
+          <div className='card mb-5 mb-sm-0' style={{width: '18rem', height: '13rem'}}>
+            <h4 className='card-title text-white bg-secondary mb-3 p-2'>Social Media</h4>
+            <p className='card-text'>Follow our social media for current events</p>
+            <Social />
+          </div>
+          <div className='card' style={{width: '18rem'}}>
+            <h4 className='card-title text-white bg-secondary mb-3 p-2'>{data.allMarkdownRemark.edges[0].node.frontmatter.title}</h4>
+            {/* <ul className="list-group list-group-flush"> */}
+              <span className='align-items-left' dangerouslySetInnerHTML={{__html: `${data.allMarkdownRemark.edges[0].node.html}`}} />
+            {/* </ul> */}
+            
+          </div>
         </div>
-    </Container>
-  </Layout>
-)
 
-export default IndexPage
+        <div style= {{ margin: `5rem 0rem` }}>
+          <Carousel />
+        </div>
+        <hr />
+        <div style={{ margin: `5rem 4rem 5rem 3rem` }}>
+          <h1>Our Mission</h1>
+          <p>Our mission is to encourage churches in several nations, by support and training in evangelism and discipleship, to achieve the Great Commission.</p>
+        </div>
+        <hr />
+          <div style={{ maxWidth: `400px`, margin: '4rem auto' }}>
+            <Image />
+          </div>
+      </Container>
+    </Layout>
+  )
+}
+
+
+export const query = graphql`
+  {
+    allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "events"}}}) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`
