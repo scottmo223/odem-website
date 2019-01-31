8f2d452be0bead36e7cc2ee248afecc193152d3f
Scott Morales <morales.scott@gmail.com>
2019-01-31 12:44:14 -0600

    Update icon

--- src/components/seo.js
+++ src/components/seo.js
@@ -11,56 +11,62 @@ function SEO({ description, lang, meta, keywords, title }) {
         const metaDescription =
           description || data.site.siteMetadata.description
         return (
-          <Helmet
-            htmlAttributes={{
-              lang,
-            }}
-            title={title}
-            titleTemplate={`%s | ${data.site.siteMetadata.title}`}
-            meta={[
-              {
-                name: `description`,
-                content: metaDescription,
-              },
-              {
-                property: `og:title`,
-                content: title,
-              },
-              {
-                property: `og:description`,
-                content: metaDescription,
-              },
-              {
-                property: `og:type`,
-                content: `website`,
-              },
-              {
-                name: `twitter:card`,
-                content: `summary`,
-              },
-              {
-                name: `twitter:creator`,
-                content: data.site.siteMetadata.author,
-              },
-              {
-                name: `twitter:title`,
-                content: title,
-              },
-              {
-                name: `twitter:description`,
-                content: metaDescription,
-              },
-            ]
-              .concat(
-                keywords.length > 0
-                  ? {
-                      name: `keywords`,
-                      content: keywords.join(`, `),
-                    }
-                  : []
-              )
-              .concat(meta)}
-          />
+          <>
+            <Helmet
+              htmlAttributes={{
+                lang,
+              }}
+              title={title}
+              titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+
+              meta={[
+                {
+                  name: `description`,
+                  content: metaDescription,
+                },
+                {
+                  property: `og:title`,
+                  content: title,
+                },
+                {
+                  property: `og:description`,
+                  content: metaDescription,
+                },
+                {
+                  property: `og:type`,
+                  content: `website`,
+                },
+                {
+                  name: `twitter:card`,
+                  content: `summary`,
+                },
+                {
+                  name: `twitter:creator`,
+                  content: data.site.siteMetadata.author,
+                },
+                {
+                  name: `twitter:title`,
+                  content: title,
+                },
+                {
+                  name: `twitter:description`,
+                  content: metaDescription,
+                },
+              ]
+                .concat(
+                  keywords.length > 0
+                    ? {
+                        name: `keywords`,
+                        content: keywords.join(`, `),
+                      }
+                    : []
+                )
+                .concat(meta)}
+            />
+            <Helmet>
+              <link rel="apple-touch-icon" sizes="160x160" href="src/image/open_doors_logo_globe160.png" />
+            </Helmet>
+          </>
         )
       }}
     />
