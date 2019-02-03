b657d05b75f08d583eae975f95573dbb58ec153c
Scott Morales <morales.scott@gmail.com>
2019-02-03 02:03:49 -0600

    Undo last commit - comment out icon in config file because it broke it

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        //icon: `src/images/open_doors_logo_globe160.png`, 
+        icon: `src/images/open_doors_logo_globe160.png`, 
       },
     },
     {
