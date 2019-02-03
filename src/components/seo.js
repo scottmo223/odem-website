6e931b3af17c65d2b694b3d7d642f21b215a64ba
Scott Morales <morales.scott@gmail.com>
2019-02-03 01:59:43 -0600

    Forgot to add correct file locations

--- src/components/seo.js
+++ src/components/seo.js
@@ -64,13 +64,14 @@ function SEO({ description, lang, meta, keywords, title }) {
                 .concat(meta)}
             />
             <Helmet>
-              <link rel="icon" type="image/png" href="/favicon.png" />
-              <link rel="apple-touch-icon" href="/touch-icon-iphone.png" />
-              <link rel="apple-touch-icon" sizes="152x152" href="/touch-icon-ipad.png" />
-              <link rel="apple-touch-icon" sizes="167x167" href="/touch-icon-ipad-retina.png" />
-              <link rel="apple-touch-icon" sizes="180x180" href="/touch-icon-iphone-retina.png" />
-              <link rel="icon" sizes="192x192" href="/android-icon.png" />
-              <meta name="msapplication-square310x310logo" content="/microsoft-icon.png" />
+              <link rel="apple-touch-icon" sizes="180x180" href="src/image/open_doors_logo_globe180.png" />
+              <link rel="icon" type="image/png" href="src/image/favicon.png" />
+              <link rel="apple-touch-icon" href="src/image/touch-icon-iphone.png" />
+              <link rel="apple-touch-icon" sizes="152x152" href="src/image/touch-icon-ipad.png" />
+              <link rel="apple-touch-icon" sizes="167x167" href="src/image/touch-icon-ipad-retina.png" />
+              <link rel="apple-touch-icon" sizes="180x180" href="src/image/touch-icon-iphone-retina.png" />
+              <link rel="icon" sizes="192x192" href="src/image/android-icon.png" />
+              <meta name="msapplication-square310x310logo" content="src/image/microsoft-icon.png" />
             </Helmet>
           </>
         )
